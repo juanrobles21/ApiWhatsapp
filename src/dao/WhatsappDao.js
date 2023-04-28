6d0ebd829d747266b58c97b8429d16f902595363
juanrobles21 <juan.roblesa@usantoto.edu.co>
2023-04-28 15:34:54 -0500

    ApiWhatsapp pruebita

--- src/dao/WhatsappDao.js
+++ src/dao/WhatsappDao.js
@@ -12,7 +12,20 @@ Object.defineProperty(exports, "__esModule", { value: true });
 class WhatsappDao {
     static VerifyToken(req, res) {
         return __awaiter(this, void 0, void 0, function* () {
-            res.send("Hola verificarToken");
+            try {
+                const accessToken = "R3849322SJFDK3953DJDJNFW53259283";
+                const token = req.query["hub.verify_token"];
+                const challenge = req.body["hub.challenge"];
+                if (challenge != null && token != null && token == accessToken) {
+                    res.send(challenge);
+                }
+                else {
+                    res.status(400).send();
+                }
+            }
+            catch (e) {
+                res.status(400).send();
+            }
         });
     }
     static ReceivedMenssage(req, res) {
